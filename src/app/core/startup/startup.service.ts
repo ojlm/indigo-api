3a03cb7cbc38832ce08dfa4928da8dea491a60ec
zhengshaodong <ngxcorpio@gmail.com>
2018-06-09 17:47:07 +0800

    add menus

--- src/app/core/startup/startup.service.ts
+++ src/app/core/startup/startup.service.ts
@@ -115,9 +115,9 @@ export class StartupService {
     // https://github.com/angular/angular/issues/15088
     return new Promise((resolve, reject) => {
       // http
-      // this.viaHttp(resolve, reject)
+      this.viaHttp(resolve, reject)
       // mock：请勿在生产环境中这么使用，viaMock 单纯只是为了模拟一些数据使脚手架一开始能正常运行
-      this.viaMock(resolve, reject)
+      // this.viaMock(resolve, reject)
     })
   }
 }
