9ece96a2d228334159027954d5d2514b0d14da92
zhengshaodong <ngxcorpio@gmail.com>
2018-08-16 19:06:40 +0800

    add basic field translate item

--- src/app/layout/indigo/header/components/new.component.ts
+++ src/app/layout/indigo/header/components/new.component.ts
@@ -10,10 +10,10 @@ import { SettingsService } from '@delon/theme'
       <i class="anticon anticon-plus"></i>
     </div>
     <div nz-menu class="width-sm">
-      <div nz-menu-item (click)="newGroup()"><i class="anticon anticon-coffee mr-sm"></i>{{'menu-new-group' | translate}}</div>
+      <div nz-menu-item (click)="newGroup()"><i class="anticon anticon-coffee mr-sm"></i>{{'new-group' | translate}}</div>
       <li nz-menu-divider></li>
-      <div nz-menu-item (click)="newProject()"><i class="anticon anticon-coffee mr-sm"></i>{{'menu-new-project' | translate}}</div>
-      <div nz-menu-item (click)="newJob()"><i class="anticon anticon-coffee mr-sm"></i>{{'menu-new-job' | translate}}</div>
+      <div nz-menu-item (click)="newProject()"><i class="anticon anticon-coffee mr-sm"></i>{{'new-project' | translate}}</div>
+      <div nz-menu-item (click)="newJob()"><i class="anticon anticon-coffee mr-sm"></i>{{'new-job' | translate}}</div>
     </div>
   </nz-dropdown>
   `,
