389d1fee17a39e3e8284417d999010ed229eac60
zhengshaodong <ngxcorpio@gmail.com>
2018-08-31 15:42:20 +0800

    1. affix url row  2.  handle params change event

--- src/app/routes/case/case-model/case-model.component.ts
+++ src/app/routes/case/case-model/case-model.component.ts
@@ -12,10 +12,12 @@ import { searchToObj } from '../../../util/urlutils'
 
 @Component({
   selector: 'app-case-model',
+  styles: ['.affixed {zoom:1.1;background-color:white;box-shadow:0px 0px 10px wheat;}'],
   templateUrl: './case-model.component.html',
 })
 export class CaseModelComponent implements OnInit {
 
+  isAffixed = false
   @Input()
   get data() { return this.case }
   set data(val: Case) {
@@ -39,6 +41,10 @@ export class CaseModelComponent implements OnInit {
     private i18nService: I18NService,
   ) { }
 
+  onAffixChange(status: boolean) {
+    this.isAffixed = status
+  }
+
   urlChange() {
     let urlStr = this.case.request.rawUrl
     try {
@@ -52,7 +58,11 @@ export class CaseModelComponent implements OnInit {
         if (url.port) {
           this.case.request.port = parseInt(url.port, 10)
         } else {
-          this.case.request.port = 80
+          if (urlStr.startsWith('https://')) {
+            this.case.request.port = 443
+          } else {
+            this.case.request.port = 80
+          }
         }
         this.case.request.urlPath = url.pathname
         const searchObj = searchToObj(url.search.substr(1))
@@ -73,8 +83,39 @@ export class CaseModelComponent implements OnInit {
     }
   }
 
-  csModelChange() {
-
+  paramsChange() {
+    if (this.case.request.query.length > 0) {
+      let search = '?'
+      this.case.request.query.forEach(item => {
+        if (item.enabled) {
+          search += `${item.key || ''}=${item.value || ''}&`
+        }
+      })
+      search = search.substr(0, search.length - 1)
+      try {
+        const rawUrl = this.case.request.rawUrl
+        if (rawUrl && !rawUrl.startsWith('?')) {
+          let hasProtocol = true
+          let url: URL
+          if (!(rawUrl.startsWith('http://') || rawUrl.startsWith('https://'))) {
+            hasProtocol = false
+            url = new URL(`http://${rawUrl}`)
+          } else {
+            url = new URL(rawUrl)
+          }
+          url.search = search
+          if (hasProtocol) {
+            this.case.request.rawUrl = url.toString()
+          } else {
+            this.case.request.rawUrl = url.toString().replace('http://', '').replace('https://', '')
+          }
+        } else {
+          this.case.request.rawUrl = search
+        }
+      } catch (error) {
+        console.error(error)
+      }
+    }
   }
 
   send() {
