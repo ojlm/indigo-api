dae3ed632ceedb8f712adf43950e6206cbe8c549
zhengshaodong <ngxcorpio@gmail.com>
2018-09-05 09:23:33 +0800

    embed swagger-editor in project-openapi component

--- proxy.conf.js
+++ proxy.conf.js
@@ -1,24 +1,34 @@
 const PROXY_CONFIG = [{
-    context: [
-      "/api/ws",
-    ],
-    target: "ws://localhost:9000",
-    pathRewrite: {
-      '^/api': '/api'
-    },
-    ws: true,
-    secure: false
+  context: [
+    "/api/ws",
+  ],
+  target: "ws://localhost:9000",
+  pathRewrite: {
+    '^/api': '/api'
   },
-  {
-    context: [
-      "/api",
-    ],
-    target: "http://localhost:9000",
-    pathRewrite: {
-      '^/api': '/api'
-    },
-    secure: false
-  }
+  ws: true,
+  secure: false
+},
+{
+  context: [
+    "/api",
+  ],
+  target: "http://localhost:9000",
+  pathRewrite: {
+    '^/api': '/api'
+  },
+  secure: false
+},
+{
+  context: [
+    "/openapi",
+  ],
+  target: "http://localhost:9000",
+  pathRewrite: {
+    '^/openapi': '/openapi'
+  },
+  secure: false
+}
 ]
 
 module.exports = PROXY_CONFIG
--- src/app/routes/project/project-openapi/project-openapi.component.ts
+++ src/app/routes/project/project-openapi/project-openapi.component.ts
@@ -1,10 +1,9 @@
 import { Location } from '@angular/common'
-import { Component, OnInit } from '@angular/core'
+import { AfterViewInit, Component, ElementRef, HostListener, OnInit } from '@angular/core'
 import { FormBuilder } from '@angular/forms'
 import { ActivatedRoute, Router } from '@angular/router'
 import { MonacoService } from '@core/config/monaco.service'
 import { NzMessageService } from 'ng-zorro-antd'
-import { DiffEditorModel } from 'ngx-monaco-editor'
 
 import { GroupService } from '../../../api/service/group.service'
 import { ProjectService } from '../../../api/service/project.service'
@@ -12,26 +11,21 @@ import { ProjectService } from '../../../api/service/project.service'
 @Component({
   selector: 'app-project-openapi',
   templateUrl: './project-openapi.component.html',
+  styles: [`
+    .openapi-ifame {
+      width: 100%;
+    }
+  `]
 })
-export class ProjectOpenapiComponent implements OnInit {
+export class ProjectOpenapiComponent implements OnInit, AfterViewInit {
 
   group: string
   project: string
   submitting = false
-
-  editorOptions = this.monacoService.getJavascriptOption()
-  code = 'function x() {\nconsole.log("Hello world!");\n}'
-  options = {
-    theme: 'vs-dark'
-  }
-  originalModel: DiffEditorModel = {
-    code: 'heLLo world!',
-    language: 'text/plain'
-  }
-  modifiedModel: DiffEditorModel = {
-    code: 'hello orlando!',
-    language: 'text/plain'
-  }
+  ifameHeight = `${window.outerHeight - 50}px`
+  loading = true
+  editor: any
+  spec = ''
 
   constructor(
     private fb: FormBuilder,
@@ -42,8 +36,13 @@ export class ProjectOpenapiComponent implements OnInit {
     private route: ActivatedRoute,
     private location: Location,
     private monacoService: MonacoService,
+    private el: ElementRef<HTMLElement>,
   ) { }
 
+  @HostListener('window:resize')
+  _resize() {
+    this.ifameHeight = `${window.outerHeight - 50}px`
+  }
 
   ngOnInit(): void {
     this.route.paramMap.subscribe(param => {
@@ -51,5 +50,28 @@ export class ProjectOpenapiComponent implements OnInit {
       this.project = param.get('project')
       console.log(this.group, this.project)
     })
+  }
+
+  setEditorValue(value: string) {
+    this.editor['specActions'].updateSpec(value)
+  }
+
+  handleEditorValueChange() {
+    const newValue = this.editor.getState().getIn(['spec', 'spec'])
+    console.log(newValue)
+  }
+
+  ngAfterViewInit(): void {
+    const iframe = this.el.nativeElement.getElementsByTagName('iframe')[0]
+    const iWindow = iframe.contentWindow
+    const timerId = window.setInterval((() => {
+      const editor = iWindow['editor']
+      if (editor) {
+        this.loading = false
+        this.editor = editor
+        this.editor.getStore().subscribe(this.handleEditorValueChange.bind(this))
+        window.clearInterval(timerId)
+      }
+    }).bind(this), 100)
   }
 }
