4f7dc9551058d481a4a2b8ccde50c0603de52388
zhengshaodong <ngxcorpio@gmail.com>
2018-09-20 08:59:56 +0800

    about assertion-list component

--- src/app/routes/case/assertion-list/assertion-list.component.ts
+++ src/app/routes/case/assertion-list/assertion-list.component.ts
@@ -1,36 +1,68 @@
 import { Location } from '@angular/common'
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core'
 import { ActivatedRoute, Router } from '@angular/router'
-import { MonacoService } from '@core/config/monaco.service'
 import { NzMessageService } from 'ng-zorro-antd'
 
-import { CaseService } from '../../../api/service/case.service'
 import { Assertion } from '../../../model/es.model'
 
 @Component({
   selector: 'app-assertion-list',
   templateUrl: './assertion-list.component.html',
+  styles: [`
+    .assertion-list {
+      overflow: auto;
+    }
+    .assertion-list::-webkit-scrollbar {
+      width: 5px;
+    }
+    .assertion-list::-webkit-scrollbar-track {
+      -webkit-box-shadow: inset 0 0 6px rgba(0,0,0,0.3);
+      border-radius: 1px;
+    }
+    .assertion-list::-webkit-scrollbar-thumb {
+      border-radius: 1px;
+      -webkit-box-shadow: inset 0 0 3px rgba(0,0,0,0.7);
+    }
+  `]
 })
 export class AssertionListComponent implements OnInit {
 
+  editorFullHeight = '470px'
+  items: AssertionItem[] = []
   @Input() assertions: Assertion[] = []
   @Input()
   get data() {
-    return []
+    return this.items
   }
-  set data(objs: any[]) {
+  set data(value: AssertionItem[]) {
+    if (value && value.length > 0) {
+      this.items = value
+    }
   }
   @Output()
-  dataChange = new EventEmitter<any[]>()
+  dataChange = new EventEmitter<AssertionItem[]>()
 
   constructor(
-    private caseService: CaseService,
     private msgService: NzMessageService,
     private router: Router,
     private route: ActivatedRoute,
     private location: Location,
-    private monocoService: MonacoService,
   ) { }
+
+  addItem() {
+    this.items.push({ path: '', operator: '$eq', value: '' })
+    this.modelChange()
+  }
+
+  copy(item: AssertionItem, index: number) {
+    this.items.push({ ...item })
+    this.modelChange()
+  }
+
+  remove(item: AssertionItem, index: number) {
+    this.items.splice(index, 1)
+    this.modelChange()
+  }
 
   modelChange() {
     this.dataChange.emit(this.data)
@@ -38,4 +70,10 @@ export class AssertionListComponent implements OnInit {
 
   ngOnInit(): void {
   }
+}
+
+export interface AssertionItem {
+  path?: string
+  operator?: string
+  value?: string
 }
