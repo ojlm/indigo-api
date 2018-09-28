a091de4236e6b0df9546b8ccb0ee5342eecc6ce8
zhengshaodong <ngxcorpio@gmail.com>
2018-09-28 19:00:48 +0800

    group-jobs component

--- src/app/routes/group/group-jobs/group-jobs.component.ts
+++ src/app/routes/group/group-jobs/group-jobs.component.ts
@@ -1,29 +1,63 @@
-import { Location } from '@angular/common'
 import { Component, OnInit } from '@angular/core'
-import { FormBuilder } from '@angular/forms'
-import { Router } from '@angular/router'
+import { ActivatedRoute, Router } from '@angular/router'
 import { NzMessageService } from 'ng-zorro-antd'
 
-import { GroupService } from '../../../api/service/group.service'
-import { Group } from '../../../model/es.model'
+import { JobService } from '../../../api/service/job.service'
+import { Job } from '../../../model/es.model'
+import { PageSingleModel } from '../../../model/page.model'
 
 @Component({
   selector: 'app-group-jobs',
   templateUrl: './group-jobs.component.html',
 })
-export class GroupJobsComponent implements OnInit {
+export class GroupJobsComponent extends PageSingleModel implements OnInit {
 
-  group: Group = {}
+  groupId: string
+  loading = false
+  items: Job[] = []
+
   constructor(
-    private fb: FormBuilder,
-    private groupService: GroupService,
-    private msgService: NzMessageService,
+    private jobService: JobService,
+    private msg: NzMessageService,
     private router: Router,
-    private location: Location,
-  ) { }
-
-  ngOnInit(): void {
+    private route: ActivatedRoute,
+  ) {
+    super()
   }
 
+  goGroup(item: Job) {
+    this.router.navigateByUrl(`/${item.group}`)
+  }
 
+  goProject(item: Job) {
+    this.router.navigateByUrl(`/${item.group}/${item.project}`)
+  }
+
+  goItem(item: Job) {
+    this.router.navigateByUrl(`/${item.group}/${item._id}`)
+  }
+
+  goSettings(item: Job) {
+    this.router.navigateByUrl(`/job/${item.group}/${item.project}/${item._id}`)
+  }
+
+  loadData() {
+    this.loading = true
+    this.jobService.query(this.toPageQuery()).subscribe(res => {
+      this.items = res.data.list
+      this.pageTotal = res.data.total
+      this.loading = false
+    }, err => this.loading = false)
+  }
+
+  pageChange() {
+    this.loadData()
+  }
+
+  ngOnInit() {
+    this.route.paramMap.subscribe(param => {
+      this.groupId = param.get('group')
+      this.loadData()
+    })
+  }
 }
