3df759ea04dc020b8330e03a904dec93fba4ba42
zhengshaodong <ngxcorpio@gmail.com>
2018-09-29 08:52:07 +0800

    add ngx-charts

--- src/app/routes/report/report.module.ts
+++ src/app/routes/report/report.module.ts
@@ -1,12 +1,12 @@
 import { CommonModule } from '@angular/common'
 import { NgModule } from '@angular/core'
 import { SharedModule } from '@shared/shared.module'
+import { NgxChartsModule } from '@swimlane/ngx-charts'
 import { MonacoEditorModule } from 'ngx-monaco-editor'
 
 import { ScenarioModule } from '../scenario/scenario.module'
 import { JobReportModelComponent } from './job-report-model/job-report-model.component'
 import { ReportRoutingModule } from './report-routing.module'
-
 
 const COMPONENT = [
   JobReportModelComponent,
@@ -20,6 +20,7 @@ const COMPONENT_NOROUNT = []
     MonacoEditorModule,
     SharedModule,
     ReportRoutingModule,
+    NgxChartsModule,
     ScenarioModule
   ],
   exports: [...COMPONENT],
