a85b8ef1dbf05f2694b42608c9b20d8317755181
zhengshaodong <ngxcorpio@gmail.com>
2018-10-20 15:53:52 +0800

    update color schema of home chart

--- src/app/routes/home/home.component.ts
+++ src/app/routes/home/home.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core'
-import { CaseService } from 'app/api/service/case.service'
+import { Component, HostListener, OnInit } from '@angular/core'
+import { AggsItem, CaseService } from 'app/api/service/case.service'
 
 import { NameValue } from '../report/job-report-model/job-report-model.component'
 
@@ -9,25 +9,61 @@ import { NameValue } from '../report/job-report-model/job-report-model.component
 })
 export class HomeComponent implements OnInit {
 
+  level = 'group'
+  height = `${window.innerHeight - 150}px`
   title = ['🌇', '🌆', '🏙', '🌃', '🌉', '🌌', '🌠', '🎆', '🌈', '🌅', '🎑', '🏞']
   results: NameValue[] = [{ name: 'indigo', value: 0 }]
   view: any[] = undefined
   colorScheme = {
-    domain: ['lightslategray', 'snow', 'whitesmoke']
+    domain: [
+      '#e0f7fa', '#b2ebf2', '#80deea', '#4dd0e1', '#26c6da', '#00bcd4', '#00acc1', '#0097a7', '#00838f', '#006064'
+    ]
   }
+  groupMap: { [k: string]: AggsItem } = {}
+  group = ''
+  projectMap: { [k: string]: AggsItem } = {}
 
+  @HostListener('window:resize')
+  resize() {
+    this.height = `${window.innerHeight - 150}px`
+  }
   constructor(
     private caseService: CaseService,
   ) {
+    this.loadGroupData()
+  }
+
+  loadGroupData() {
+    this.level = 'group'
+    this.group = ''
     this.caseService.aggs({}).subscribe(res => {
+      const map = {}
       this.results = res.data.map(item => {
+        map[item.id] = item
         return { name: item.id, value: item.count }
       })
+      this.groupMap = map
     })
   }
 
-  onSelect(item: any) {
-    console.log(item)
+  loadGroupProjectData(group: string) {
+    this.caseService.aggs({ group: group }).subscribe(res => {
+      const map = {}
+      this.results = res.data.map(item => {
+        map[item.id] = item
+        return { name: item.id, value: item.count }
+      })
+      this.projectMap = map
+    })
+  }
+
+  onSelect(item: NameValue) {
+    if ('group' === this.level) {
+      const aggItem = this.groupMap[item.name]
+      this.level = 'project'
+      this.group = aggItem.id
+      this.loadGroupProjectData(aggItem.id)
+    }
   }
 
   shuffle() {
