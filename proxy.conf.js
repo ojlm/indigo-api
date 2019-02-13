8a4d4a99e0a12c9bc237aad92d78790bfddb09b7
zhengshaodong <ngxcorpio@gmail.com>
2019-02-13 11:42:29 +0800

    Add changeOrigin option

--- proxy.conf.js
+++ proxy.conf.js
@@ -1,34 +1,37 @@
 const PROXY_CONFIG = [{
-  context: [
-    "/api/ws",
-  ],
-  target: "ws://localhost:9000",
-  pathRewrite: {
-    '^/api/': '/api/'
+    context: [
+      "/api/ws",
+    ],
+    target: "ws://localhost:9000",
+    pathRewrite: {
+      '^/api/': '/api/'
+    },
+    ws: true,
+    changeOrigin: true,
+    secure: false
   },
-  ws: true,
-  secure: false
-},
-{
-  context: [
-    "/api/",
-  ],
-  target: "http://localhost:9000",
-  pathRewrite: {
-    '^/api/': '/api/'
+  {
+    context: [
+      "/api/",
+    ],
+    target: "http://localhost:9000",
+    pathRewrite: {
+      '^/api/': '/api/'
+    },
+    changeOrigin: true,
+    secure: false
   },
-  secure: false
-},
-{
-  context: [
-    "/openapi",
-  ],
-  target: "http://localhost:9000",
-  pathRewrite: {
-    '^/openapi': '/openapi'
-  },
-  secure: false
-}
+  {
+    context: [
+      "/openapi",
+    ],
+    target: "http://localhost:9000",
+    pathRewrite: {
+      '^/openapi': '/openapi'
+    },
+    changeOrigin: true,
+    secure: false
+  }
 ]
 
 module.exports = PROXY_CONFIG
