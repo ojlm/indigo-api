d9da9832fda64f10625b5ba0e03bfa9be5b61ad5
zhengshaodong <ngxcorpio@gmail.com>
2019-05-10 17:37:23 +0800

    Support selecting dubbo and sql request item

--- src/app/routes/scenario/select-step/select-step.component.ts
+++ src/app/routes/scenario/select-step/select-step.component.ts
@@ -1,16 +1,22 @@
-import { Location } from '@angular/common'
 import { Component, HostListener, Input, OnInit } from '@angular/core'
-import { ActivatedRoute, Router } from '@angular/router'
 import { GroupProjectSelectorModel } from '@shared/group-project-selector/group-project-selector.component'
+import { dubboRequestSignature, DubboService, QueryDubboRequest } from 'app/api/service/dubbo.service'
+import { QuerySqlRequest, sqlRequestSignature, SqlService } from 'app/api/service/sql.service'
 import { ApiRes } from 'app/model/api.model'
 import { PageSingleModel } from 'app/model/page.model'
 import { CaseModelComponent } from 'app/routes/case/case-model/case-model.component'
+import { DubboPlaygroundComponent } from 'app/routes/dubbo/dubbo-playground/dubbo-playground.component'
+import { SqlPlaygroundComponent } from 'app/routes/sql/sql-playground/sql-playground.component'
 import { calcDrawerWidth } from 'app/util/drawer'
 import { NzDrawerService, NzMessageService } from 'ng-zorro-antd'
 import { Subject } from 'rxjs'
 
 import { CaseService, QueryCase } from '../../../api/service/case.service'
-import { caseToScenarioStep, ScenarioStepType } from '../../../api/service/scenario.service'
+import {
+  caseToScenarioStep,
+  dubboRequestToScenarioStep,
+  sqlRequestToScenarioStep,
+} from '../../../api/service/scenario.service'
 import { Case, ContextOptions, DubboRequest, ScenarioStep, SqlRequest } from '../../../model/es.model'
 
 @Component({
@@ -23,9 +29,13 @@ import { Case, ContextOptions, DubboRequest, ScenarioStep, SqlRequest } from '..
       width: 100%;
     }
     .line .title {
-      padding-left: 10px;
+      padding-left: 4px;
       font-size: smaller;
       color: lightgray;
+    }
+    .line .title .summary{
+      color: black;
+      margin-right: 4px;
     }
     .line .tail-labels {
       float: right;
@@ -57,6 +67,16 @@ export class SelectStepComponent implements OnInit {
   searchHttpStepModel: QueryCase = {}
   searchHttpStepSubject: Subject<QueryCase>
 
+  dubboItems: DubboRequest[] = []
+  dubboItemsPage = new PageSingleModel()
+  searchDubboStepModel: QueryDubboRequest = {}
+  searchDubboStepSubject: Subject<QueryDubboRequest>
+
+  sqlItems: SqlRequest[] = []
+  sqlItemsPage = new PageSingleModel()
+  searchSqlStepModel: QuerySqlRequest = {}
+  searchSqlStepSubject: Subject<QuerySqlRequest>
+
   _ctxOptions: ContextOptions = {}
   @Input()
   set ctxOptions(val: ContextOptions) {
@@ -73,11 +93,10 @@ export class SelectStepComponent implements OnInit {
 
   constructor(
     private caseService: CaseService,
+    private dubboService: DubboService,
+    private sqlService: SqlService,
     private drawerService: NzDrawerService,
     private msgService: NzMessageService,
-    private router: Router,
-    private route: ActivatedRoute,
-    private location: Location,
   ) {
     this.searchGroupProject = {
       group: this.group,
@@ -86,7 +105,11 @@ export class SelectStepComponent implements OnInit {
   }
 
   tabIndexChange() {
-    console.log(this.tabIndex)
+    if (1 === this.tabIndex && this.dubboItems.length === 0) {
+      this.searchDubboSteps()
+    } else if (2 === this.tabIndex && this.sqlItems.length === 0) {
+      this.searchSqlSteps()
+    }
   }
 
   groupProjectChange() {
@@ -95,8 +118,10 @@ export class SelectStepComponent implements OnInit {
         this.searchHttpSteps()
         break
       case 1:
+        this.searchDubboSteps()
         break
       case 2:
+        this.searchSqlSteps()
         break
     }
   }
@@ -104,14 +129,36 @@ export class SelectStepComponent implements OnInit {
   addHttpStep(item: Case) {
     if (this.onSelectSubject) {
       this.onSelectSubject.next({
-        step: {
-          id: item._id,
-          type: ScenarioStepType.CASE,
-          stored: false,
-        },
+        step: caseToScenarioStep(item),
         stepData: item
       })
     }
+  }
+
+  addDubboStep(item: DubboRequest) {
+    if (this.onSelectSubject) {
+      this.onSelectSubject.next({
+        step: dubboRequestToScenarioStep(item),
+        stepData: item
+      })
+    }
+  }
+
+  addSqlStep(item: SqlRequest) {
+    if (this.onSelectSubject) {
+      this.onSelectSubject.next({
+        step: sqlRequestToScenarioStep(item),
+        stepData: item
+      })
+    }
+  }
+
+  dubboSignature(item: DubboRequest) {
+    return dubboRequestSignature(item)
+  }
+
+  sqlSignature(item: SqlRequest) {
+    return sqlRequestSignature(item)
   }
 
   viewHttpStep(item: Case) {
@@ -120,7 +167,6 @@ export class SelectStepComponent implements OnInit {
       nzContent: CaseModelComponent,
       nzContentParams: {
         id: item._id,
-        isInDrawer: true,
         newStep: (stepData: Case) => {
           this.onSelectSubject.next({ step: caseToScenarioStep(stepData), stepData: stepData })
         },
@@ -134,10 +180,69 @@ export class SelectStepComponent implements OnInit {
           })
           this.onUpdateSubject.next({ step: caseToScenarioStep(stepData), stepData: stepData })
         },
+        isInDrawer: true,
         ctxOptions: this._ctxOptions
       },
       nzBodyStyle: {
-        padding: '0px'
+        padding: '4px'
+      },
+      nzClosable: false,
+    })
+  }
+
+  viewDubboStep(item: DubboRequest) {
+    this.drawerService.create({
+      nzWidth: this.drawerWidth,
+      nzContent: DubboPlaygroundComponent,
+      nzContentParams: {
+        id: item._id,
+        newStep: (stepData: DubboRequest) => {
+          this.onSelectSubject.next({ step: dubboRequestToScenarioStep(stepData), stepData: stepData })
+        },
+        updateStep: (stepData: DubboRequest) => {
+          this.dubboItems = this.dubboItems.map(dubboItem => {
+            if (dubboItem._id === stepData._id) {
+              return stepData
+            } else {
+              return item
+            }
+          })
+          this.onUpdateSubject.next({ step: dubboRequestToScenarioStep(stepData), stepData: stepData })
+        },
+        isInDrawer: true,
+        ctxOptions: this._ctxOptions
+      },
+      nzBodyStyle: {
+        padding: '4px'
+      },
+      nzClosable: false,
+    })
+  }
+
+  viewSqlStep(item: SqlRequest) {
+    this.drawerService.create({
+      nzWidth: this.drawerWidth,
+      nzContent: SqlPlaygroundComponent,
+      nzContentParams: {
+        id: item._id,
+        newStep: (stepData: SqlRequest) => {
+          this.onSelectSubject.next({ step: sqlRequestToScenarioStep(stepData), stepData: stepData })
+        },
+        updateStep: (stepData: SqlRequest) => {
+          this.sqlItems = this.sqlItems.map(sqlItem => {
+            if (sqlItem._id === stepData._id) {
+              return stepData
+            } else {
+              return item
+            }
+          })
+          this.onUpdateSubject.next({ step: sqlRequestToScenarioStep(stepData), stepData: stepData })
+        },
+        isInDrawer: true,
+        ctxOptions: this._ctxOptions
+      },
+      nzBodyStyle: {
+        padding: '4px'
       },
       nzClosable: false,
     })
@@ -164,14 +269,40 @@ export class SelectStepComponent implements OnInit {
     })
   }
 
+  searchDubboSteps() {
+    this.searchDubboStepSubject.next({
+      ...this.searchDubboStepModel, ...this.dubboItemsPage.toPageQuery(), ...this.searchGroupProject
+    })
+  }
+
+  searchSqlSteps() {
+    this.searchSqlStepSubject.next({
+      ...this.searchSqlStepModel, ...this.sqlItemsPage.toPageQuery(), ...this.searchGroupProject
+    })
+  }
+
   ngOnInit(): void {
-    const response = new Subject<ApiRes<Case[]>>()
-    response.subscribe(res => {
+    const httpStepsResponse = new Subject<ApiRes<Case[]>>()
+    httpStepsResponse.subscribe(res => {
       this.httpItemsPage.pageTotal = res.data.total
       this.httpItems = res.data.list
     })
-    this.searchHttpStepSubject = this.caseService.newQuerySubject(response)
+    this.searchHttpStepSubject = this.caseService.newQuerySubject(httpStepsResponse)
     this.searchHttpSteps()
+
+    const dubboStepsResponse = new Subject<ApiRes<DubboRequest[]>>()
+    dubboStepsResponse.subscribe(res => {
+      this.dubboItemsPage.pageTotal = res.data.total
+      this.dubboItems = res.data.list
+    })
+    this.searchDubboStepSubject = this.dubboService.newQuerySubject(dubboStepsResponse)
+
+    const sqlStepsResponse = new Subject<ApiRes<SqlRequest[]>>()
+    sqlStepsResponse.subscribe(res => {
+      this.sqlItemsPage.pageTotal = res.data.total
+      this.sqlItems = res.data.list
+    })
+    this.searchSqlStepSubject = this.sqlService.newQuerySubject(sqlStepsResponse)
   }
 }
 
