b1f4aa23b258493193473db26560289597137960
zhengshaodong <ngxcorpio@gmail.com>
2019-07-23 20:19:22 +0800

    Misc code

--- src/app/routes/ci/ci-event/ci-event.component.ts
+++ src/app/routes/ci/ci-event/ci-event.component.ts
@@ -1,6 +1,15 @@
 import { Component, Input, OnInit } from '@angular/core'
 import { ActivatedRoute } from '@angular/router'
-import { CiTrigger } from 'app/model/es.model'
+import { httpRequestSignature } from 'app/api/service/case.service'
+import { dubboRequestSignature } from 'app/api/service/dubbo.service'
+import { ScenarioStepType } from 'app/api/service/scenario.service'
+import { sqlRequestSignature } from 'app/api/service/sql.service'
+import { Case, CiTrigger, DubboRequest, Job, SqlRequest } from 'app/model/es.model'
+import { SelectJobComponent } from 'app/routes/scenario/select-job/select-job.component'
+import { SelectStepComponent, StepEvent } from 'app/routes/scenario/select-step/select-step.component'
+import { calcDrawerWidth } from 'app/util/drawer'
+import { NzDrawerRef, NzDrawerService } from 'ng-zorro-antd'
+import { Subject } from 'rxjs'
 
 @Component({
   selector: 'app-ci-event',
@@ -9,6 +18,12 @@ import { CiTrigger } from 'app/model/es.model'
 })
 export class CiEventComponent implements OnInit {
 
+  drawerRef: NzDrawerRef
+  drawerWidth = calcDrawerWidth(0.75)
+  readinessSubject = new Subject<StepEvent>()
+  readinessTarget: StepEvent = {}
+  job: Job = {}
+  jobSubject = new Subject<StepEvent>()
   @Input() group = ''
   @Input() project = ''
   isSending = false
@@ -24,16 +39,88 @@ export class CiEventComponent implements OnInit {
   }
 
   constructor(
+    private drawerService: NzDrawerService,
     private route: ActivatedRoute,
   ) {
+    this.readinessSubject.subscribe(event => {
+      const step = event.step
+      this.request.readiness.targetType = step.type
+      this.request.readiness.targetId = step.id
+      this.readinessTarget = event
+      this.drawerRef.close()
+    })
+    this.jobSubject.subscribe(event => {
+      this.request.targetType = event.step.type
+      this.request.targetId = event.step.id
+      this.job = event.stepData
+      this.drawerRef.close()
+    })
+  }
+
+  jobTips() {
+    if (this.job) {
+      return this.job.summary
+    } else {
+      return ''
+    }
+  }
+
+  readinessTargetTips() {
+    if (this.readinessTarget && this.readinessTarget.step) {
+      const stepData = this.readinessTarget.stepData
+      let signature = ''
+      switch (this.readinessTarget.step.type) {
+        case ScenarioStepType.CASE:
+          signature = httpRequestSignature(stepData as Case)
+          return `${stepData.summary},  ${signature}`
+        case ScenarioStepType.SQL:
+          signature = sqlRequestSignature(stepData as SqlRequest)
+          return `${stepData.summary},  ${signature}`
+        case ScenarioStepType.DUBBO:
+          signature = dubboRequestSignature(stepData as DubboRequest)
+          return `${stepData.summary},  ${signature}`
+        default:
+          return ''
+      }
+    } else {
+      return ''
+    }
   }
 
   save() {
     console.log(this.request)
   }
 
-  select() {
+  selectJob() {
+    this.drawerRef = this.drawerService.create({
+      nzWidth: this.drawerWidth,
+      nzContent: SelectJobComponent,
+      nzContentParams: {
+        group: this.group,
+        project: this.project,
+        onSelectSubject: this.jobSubject,
+      },
+      nzBodyStyle: {
+        padding: '4px'
+      },
+      nzClosable: false,
+    })
+  }
 
+  select() {
+    this.drawerRef = this.drawerService.create({
+      nzWidth: this.drawerWidth,
+      nzContent: SelectStepComponent,
+      nzContentParams: {
+        group: this.group,
+        project: this.project,
+        onSelectSubject: this.readinessSubject,
+      },
+      nzBodyStyle: {
+        padding: '4px'
+      },
+      nzClosable: false,
+    })
   }
 
   modelChange() {
@@ -41,5 +128,17 @@ export class CiEventComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    if (this.route.parent && this.route.parent.parent) {
+      // if it was created from NzDrawerService, this.route.parent will be null
+      this.route.parent.parent.params.subscribe(params => {
+        this.group = params['group']
+        this.project = params['project']
+      })
+      this.route.parent.params.subscribe(params => {
+        const docId = params['ciId']
+        if (docId) {
+        }
+      })
+    }
   }
 }
